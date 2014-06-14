12ffa924fae454505378a5c803c256643ee63249
jordiromero <jordiromero@iesebre.com>
2014-06-14 13:30:45 +0200

    commit sintesiapp3

--- sintesiapp/src/main/java/com/iesebre/sintesiapp3/sintesiapp/MapaActivity.java
+++ sintesiapp/src/main/java/com/iesebre/sintesiapp3/sintesiapp/MapaActivity.java
@@ -5,13 +5,36 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 public class MapaActivity extends Activity {
+
+    GoogleMap map;
+    private static final LatLng EL_TEU_RESTAURANT = new LatLng(40.81471, 0.515187);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        try {
+            if (map == null){
+                map=((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            }
+            Marker rest = map.addMarker(new MarkerOptions().position(EL_TEU_RESTAURANT).title("El teu restaurant"));
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(EL_TEU_RESTAURANT).zoom(16).build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
